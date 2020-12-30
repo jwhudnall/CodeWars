d9dee1da87b5c25ae51a47504d9544e5e8974024
James Hudnall <jimmyhudnall@gmail.com>
2020-12-30 14:12:39 -0500

    Initial commit for working version of game

---
+++ snakes-ladders.py
@@ -0,0 +1,83 @@
+class SnakesLadders():
+
+    def __init__(self):
+        self.p1_square = 0
+        self.p2_square = 0
+        self.player_turn = 1
+        self.gameover = False
+        self.ladders_dict = {2:38, 7:14, 8:31, 15:26, 21:42, 28:84, 36:44, 51:67, 71:91, 78:98, 87:94}
+        self.snakes_dict = {16:6, 46:25, 49:11, 62:19, 64:60, 74:53, 89:68, 92:88, 95:75, 99:80}
+
+    
+    
+    
+    def play(self, die1, die2):
+        self.die1 = die1
+        self.die2 = die2
+        self.current_turn = 1 if self.player_turn == 1 else 2
+        self.same_dice = self.die1 == self.die2
+        self.roll = die1 + die2
+        
+        def get_new_position(roll):
+            previous_square = self.p1_square if self.player_turn == 1 else self.p2_square
+            return previous_square + roll
+        
+        def update_p1_square(new_position):
+            self.p1_square = new_position
+        
+        def update_p2_square(new_position):
+            self.p2_square = new_position
+
+        def update_square(current_player, new_position):
+            return update_p1_square(new_position) if self.current_turn == 1 else update_p2_square(new_position)
+        
+        def switch_player():
+            if self.current_turn == 1:
+                self.player_turn = 2
+            else:
+                self.player_turn = 1
+                
+        def winner():
+            winner = self.current_turn
+            self.gameover = True
+            return (f'Player {winner} Wins!')
+            
+
+        def show_square(current_player):
+            return self.p1_square if self.current_turn == 1 else self.p2_square
+        
+        if not self.gameover:
+            new_position = get_new_position(self.roll)
+            update_p1_square(new_position) if self.current_turn == 1 else update_p2_square(new_position)
+
+
+            if new_position == 100:
+                update_square(self.current_turn, new_position)
+                return winner()
+
+            if new_position > 100:
+                new_position = 100 - (new_position - 100)
+                print(f'You passed 100! Your new position is {new_position}')
+                update_square(self.current_turn, new_position)
+
+            if new_position in self.ladders_dict:
+                print(f'You hit a ladder at position {new_position}')
+                new_position = self.ladders_dict[new_position]
+                print(f'Player {self.current_turn}, your new position is: {new_position}')
+                update_square(self.current_turn, new_position)
+
+            elif new_position in self.snakes_dict:
+                print(f'You hit a snake at position {new_position}')
+                new_position = self.snakes_dict[new_position]
+                print(f'Player {self.current_turn}, your new position is {new_position}')
+                update_square(self.current_turn, new_position)
+
+            else:
+                update_square(self.current_turn, new_position)
+
+            if not self.same_dice:
+                switch_player()
+
+            return f'Player {self.current_turn} is on square {show_square(self.current_turn)}'
+        else:
+            return 'Game over!'
